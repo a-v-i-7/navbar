74ad5bccbd96adfa43803ef51b48538758012ead
Avinash Kumar <avi.cool36@gmail.com>
2020-10-24 02:20:24 +0530

    facebook style nav bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar>
+        <NavItem icon="😊" />
+        <NavItem icon="😊" />
+        <NavItem icon="😊" />
+        <NavItem icon="😊" />
+        <NavItem icon="😊">
+          <Dropdown />
+        </NavItem>
+      </Navbar>
     </div>
   );
 }
 
+const Dropdown = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState(null)
+
+  const calcHeight = (el) => {
+    console.log(el.offsetHeight);
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  return (
+    <div className="dropdown" style={{ height:menuHeight }}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-primary"
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem
+            leftIcon="😊"
+            rightIcon="❤"
+            goToMenu="settings"
+            setActiveMenu={setActiveMenu}
+          >
+            item 1
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={activeMenu === "settings"}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-secondary"
+        onEnter={calcHeight}
+
+      >
+        <div className="menu">
+          <DropdownItem
+            leftIcon="😊"
+            rightIcon="❤"
+            goToMenu="main"
+            setActiveMenu={setActiveMenu}
+          >
+            secondry Item
+          </DropdownItem>
+          <DropdownItem
+            leftIcon="😊"
+            rightIcon="❤"
+            goToMenu="main"
+            setActiveMenu={setActiveMenu}
+          >
+            secondry Item
+          </DropdownItem>
+          <DropdownItem
+            leftIcon="😊"
+            rightIcon="❤"
+            goToMenu="main"
+            setActiveMenu={setActiveMenu}
+          >
+            secondry Item
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
+  );
+};
+
+const DropdownItem = ({
+  leftIcon,
+  rightIcon,
+  children,
+  goToMenu,
+  setActiveMenu,
+}) => {
+  return (
+    <a
+      href="#/"
+      className="menu-item"
+      onClick={() => goToMenu && setActiveMenu(goToMenu)}
+    >
+      <span className="icon-button"> {leftIcon}</span>
+      {children}
+      <span className="icon-right">{rightIcon}</span>
+    </a>
+  );
+};
+
+const Navbar = ({ children }) => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{children}</ul>
+    </nav>
+  );
+};
+
+const NavItem = ({ children, icon }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a
+        href="#"
+        className="icon-button"
+        onClick={() => setOpen((val) => !val)}
+      >
+        {icon}
+      </a>
+      {open && children}
+    </li>
+  );
+};
 export default App;
